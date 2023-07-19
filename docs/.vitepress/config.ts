7f358b9df5918fe9234c8d84b0430e8c5c85fa7d
luhaonan <kobeluhaonan@163.com>
2023-07-19 10:02:14 +0800

    docs: clean docs folder & change some docs config

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -3,34 +3,18 @@ import { defineConfig } from 'vitepress'
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
   base: '/bdsaas-bc/',
-  title: 'BI Chart',
-  description: 'BI-Chart 图表组件库',
+  title: 'BDSaas-bc',
+  description: 'BDSaas 业务组件库',
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
       { text: '首页', link: '/' },
-      { text: '示例', link: '/small-chart/' },
       { text: '更新日志', link: '/change-log/' },
-      { text: '进阶主题', link: '/advanced-use/' }
     ],
 
-    sidebar: [
-      {
-        text: '使用指南',
-        items: [
-          { text: '快速开始', link: '/quick-start/' },
-          { text: '示例：小图表', link: '/small-chart/' },
-          { text: '示例：大图标', link: '/big-chart/' }
-        ]
-      }
-    ],
+    sidebar: [],
 
-    socialLinks: [
-      {
-        icon: 'github',
-        link: 'https://github.com/dizuncainiao/bi-chart/tree/main'
-      }
-    ]
+    socialLinks: []
   },
   lastUpdated: true,
   vite: {
