902f903d71eea66ec878bf1199a668c1b37e2994
wangbo <781278657@qq.com>
2023-07-31 10:09:24 +0800

    feat: ✨ 图表配置框样式抽离

--- packages/bdsaas-bc/components/bc-chart-config-modal/src/data-report-page/added-page/DataReportBig.vue
+++ packages/bdsaas-bc/components/bc-chart-config-modal/src/data-report-page/added-page/DataReportBig.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="selected-content-big selected-content">
+  <div class="data-report-selected-content-big data-report-selected-content">
     <div class="tips-box">
       已添加
       <span class="gray">（{{ state.selectedList.length }}）</span>
@@ -96,92 +96,3 @@ export default {
   name: 'DataReportBig'
 }
 </script>
-
-<style scoped lang="less">
-.selected-content {
-  flex: 1;
-  height: 100%;
-  padding: 32px 40px 80px;
-  overflow-y: auto;
-  user-select: none;
-
-  .tips-box {
-    line-height: 1;
-  }
-
-  .selected-list {
-    display: grid;
-    grid-template-rows: repeat(1, 80px);
-    grid-template-columns: repeat(2, 1fr);
-    grid-gap: 12px;
-    margin-top: 32px;
-
-    .list-item {
-      position: relative;
-      display: grid;
-      place-items: center;
-      height: 80px;
-      overflow: hidden;
-      border: 1px solid #e1e4e8;
-      border-radius: 8px;
-
-      .mask-box {
-        position: absolute;
-        top: 0;
-        left: 0;
-        z-index: -1;
-        display: flex;
-        gap: 20px;
-        align-items: center;
-        justify-content: center;
-        width: 100%;
-        height: 100%;
-        background-color: rgb(0 0 0 / 60%);
-        opacity: 0;
-        transition: 0.3s;
-
-        :deep(.action-icon) {
-          font-size: 24px;
-          color: #fff;
-          cursor: pointer;
-        }
-
-        .move {
-          cursor: move;
-        }
-      }
-
-      &:hover {
-        .mask-box {
-          z-index: 2;
-          opacity: 1;
-        }
-      }
-    }
-  }
-}
-
-.gray {
-  color: #9ca6b9;
-}
-
-/* 1. 声明过渡效果 */
-.fade-move,
-.fade-enter-active,
-.fade-leave-active {
-  transition: all 0.5s cubic-bezier(0.55, 0, 0.1, 1);
-}
-
-/* 2. 声明进入和离开的状态 */
-.fade-enter-from,
-.fade-leave-to {
-  opacity: 0;
-  transform: scaleY(0.01) translate(30px, 0);
-}
-
-/* 3. 确保离开的项目被移除出了布局流
-      以便正确地计算移动时的动画效果。 */
-.fade-leave-active {
-  position: absolute;
-}
-</style>
