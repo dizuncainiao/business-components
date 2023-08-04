4f3791ffa79507ede853d4a9826a07abe4870ae3
luhaonan <kobeluhaonan@163.com>
2023-08-04 14:38:44 +0800

    fix: 🐛 优化调整音频播放的处理方式，使其更流畅的切换

--- packages/bdsaas-bc/components/bc-audio/src/index.vue
+++ packages/bdsaas-bc/components/bc-audio/src/index.vue
@@ -2,17 +2,13 @@
   <div class="bc-audio-btn" :class="{ inline }" @click="play">
     <bn-button type="primary" v-if="showBtn && !isSlot" link>{{ btnName }}</bn-button>
     <slot name="default"></slot>
-    <div class="audio-player-wrap">
-      <div id="audioPlayer"></div>
-    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, useSlots } from 'vue'
-import { Button, Message } from 'blocks-next'
-import { AudioPlayer } from '../../../_plugins/a-player'
-import 'aplayer/dist/APlayer.min.css'
+import { Button } from 'blocks-next'
+import { audioPlay, audioDestory } from './hooks/player'
 import '../style/index.less'
 
 export default defineComponent({
@@ -75,63 +71,12 @@ export default defineComponent({
 
     const isSlot = !!useSlots().default
 
-    let audioPlayer: any = null
-
-    // 播放
     const play = () => {
-      destroy()
-      if (!props.src) {
-        Message.warning('音频地址不能为空')
-        return false
-      }
-
-      if (!props.name) {
-        Message.warning('音频名称不能为空')
-        return false
-      }
-
-      if (!audioPlayer) {
-        audioPlayer = new AudioPlayer({
-          container: document.getElementById('audioPlayer'),
-          autoplay: true,
-          loop: 'none',
-          audio: [
-            {
-              name: props.name,
-              url: props.src,
-              artist: props.artist
-            }
-          ]
-        })
-      }
-
-      audioPlayer.on('canplaythrough', () => {
-        if (props.successMsg) {
-          Message.success(props.successMsg)
-        }
-      })
-
-      audioPlayer.on('ended', () => {
-        emit('afterPlay')
-        destroy()
-      })
-
-      // TODO: 播放失败情况暂不提示，不确定是否是本地跨域访问音频资源报错导致每次播放完毕都会报错
-      audioPlayer.on('error', e => {
-        emit('afterError')
-        if (props.showErrorMsg) {
-          Message.error(props.errorMsg)
-        }
-        destroy()
-      })
+      audioPlay(props, emit)
     }
 
-    // 销毁
     const destroy = () => {
-      if (audioPlayer) {
-        audioPlayer.destroy()
-      }
-      audioPlayer = null
+      audioDestory()
     }
 
     return {
