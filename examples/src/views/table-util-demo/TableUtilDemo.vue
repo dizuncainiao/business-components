b09fe6645b44e35cd1f3e3b2e92f3faadaa72a85
Kobe.Lu <kobeluhaonan@gmail.com>
2023-08-28 09:24:32 +0800

    test: 🚨 测试PageMixin的可行性，并完善使用示范

--- examples/src/views/table-util-demo/TableUtilDemo.vue
+++ examples/src/views/table-util-demo/TableUtilDemo.vue
@@ -11,10 +11,10 @@
 
 <script setup lang="ts">
 import { ref } from 'vue'
-import { BcTableUtil } from 'bdsaas-bc'
+import { BcTableUtil, BcPageMixinComposition, BcOperateComposition } from 'bdsaas-bc'
 
 const token =
-  'eyJhbGciOiJIUzUxMiJ9.eyJqdGkiOiJiZHNhYXMiLCJzdWIiOiI0MzM0OCIsImV4cCI6MTY5MTA4Mzk5Nn0.pcjoq8SMp0xDNtAf6Wku5zjpglVnLSDKpikbdSF60aQw_rwIm-E161ePJ9VC3OqMuA4Yk_wke37FQvS5UgCPDg'
+  'eyJhbGciOiJIUzUxMiJ9.eyJqdGkiOiJiZHNhYXMiLCJzdWIiOiI0MzM0OCIsImV4cCI6MTY5MzIxMzc1OH0.Bq_Z69Bsu9UKI891teLCuiBozFCwPIE5l_SA8z4nAdkrMEWrBRycN8vJiBc5fG3dylSt_wTjYu5e9Nsu6nw8Vw'
 localStorage.setItem('_BDSAAS_TOKEN', token)
 
 const columns = ref([
@@ -37,18 +37,41 @@ const queryForm = {
 
 const url = '/bdsaas/ajax/item/companyItem/selectALLBySkuCriteria.do'
 
-const { tableData, pageConfig, search } = BcTableUtil(url, queryForm, {
-  method: 'postJson',
-  headers: {
-    'content-type': 'application/x-www-form-urlencoded;charset=UTF-8'
-  },
-  paramPageKey: 'page',
-  paramPageSizeKey: 'pagesize',
-  totalKey: 'pagination.total',
-  recordsKey: 'result',
-  pageSizeKey: 'pagination.pagesize',
-  pageNumKey: 'pagination.page'
+// 由于我们的后端规范并不是遵循RestFul架构，所以操作的url需要手动传递，这里只是为了演示
+// 此Composition API主要是为了组合方便大家不关注接口回调，只关注数据和操作方法
+// 由于业务的不同，此PageMixin没有多余定制过多内容，后续可继续按照业务需求进行定制
+// 或者自己使用BcTableUtil + BcOperateComposition进行组合使用，定制属于自己的PageMixin
+
+console.log(BcTableUtil, BcOperateComposition)
+
+const { tableData, pageConfig, search, handleCancelOperate, handleCreate, handleDelete } = BcPageMixinComposition({
+  url,
+  queryForm,
+  options: {
+    method: 'postJson',
+    headers: {
+      'content-type': 'application/x-www-form-urlencoded;charset=UTF-8'
+    },
+    paramPageKey: 'page',
+    paramPageSizeKey: 'pagesize',
+    totalKey: 'pagination.total',
+    recordsKey: 'result',
+    pageSizeKey: 'pagination.pagesize',
+    pageNumKey: 'pagination.page'
+  }
+}, {
+  url: '示范新增参数配置',
+  type: 'create',
+  operateForm: {},
+  options: {}
+}, {
+  url: '示范删除参数配置',
+  type: 'del',
+  operateForm: {},
+  options: {}
 })
+
+console.log(handleCreate, handleDelete, handleCancelOperate)
 
 setTimeout(() => {
   delete queryForm.allItem
