077fcdc7243567384c537def9ccb357a53a8fad4
Kobe.Lu <kobeluhaonan@gmail.com>
2023-08-30 10:39:46 +0800

    fix: 🐛 修复下载组件和音频播放组件在组件外部调用暴露方法时的bug

--- examples/src/views/audio-play-demo/AudioPlayDemo.vue
+++ examples/src/views/audio-play-demo/AudioPlayDemo.vue
@@ -1,18 +1,30 @@
 <template>
   <div>
+    <button @click="changeSrcAndName">change</button>
     <BcAudio
       ref="BcAudioRef"
-      src="https://demo.bdsaas.cn/call_record/2023/07/06/1564/bf1b017f37d8495d8f824e57949d4fa8..wav"
-      name="测试录音"
+      :src="src"
+      :name="name"
+      :showBtn="false"
     />
   </div>
 </template>
 
 <script setup lang="ts">
-import { defineComponent } from 'vue'
+import { ref } from 'vue'
 import { BcAudio } from 'bdsaas-bc'
 
 defineOptions({
   name: 'AudioPlayDemo'
 })
+const BcAudioRef = ref()
+const src = ref('')
+const name = ref('')
+
+const changeSrcAndName = () => {
+  src.value = 'https://demo.bdsaas.cn/call_record/2023/07/06/1564/bf1b017f37d8495d8f824e57949d4fa8..wav'
+  name.value = '金卡跨境电商看见你电脑开机啊苏卡达缴纳卡机奶萨到哪科技'
+  BcAudioRef.value.play()
+}
+
 </script>
